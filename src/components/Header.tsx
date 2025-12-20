9135c845e60e895b9f831aded65eece2c708d88a
isgin01 <isgin01@gmail.com>
2025-12-20 22:00:40 +0500

    Simplify <Header />
    <Section />, <Flower />, and <Logo /> are not used anywhere else except
    the <Header /> so it is reasonable to move them all into a single
    file for simplicity.
    
    Provide better naming for components.
    
    Simplify components' logic.
    
    Create a separate <Logo />. Using <Flower /> for showing logo made
    <Flower /> unnecessary complicated. Introducing another component
    is more simple.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,63 @@
-import { HeaderImgElement, HeaderTextElement } from "./HeaderElements"
+import Image from "next/image"
 
 export default function Header() {
     return (
         <header className="flex flex-row bg-midnight w-full h-28 sticky justify-center font-primary">
-            <HeaderImgElement imageSource="" />
-            <HeaderTextElement
-                backgroundImage="/img/nav/first-nav-bg.png"
-                text="About cape⁴labs"
-            />
-            <HeaderImgElement imageSource="/img/nav/first-nav-flower.png" />
-            <HeaderTextElement
-                backgroundImage="/img/nav/second-nav-bg.png"
-                text="About"
-            />
-            <HeaderImgElement imageSource="/img/nav/second-nav-flower.png" />
-            <HeaderTextElement
-                backgroundImage="/img/nav/third-nav-bg.png"
-                text="Where we can help you"
-            />
-            <HeaderImgElement imageSource="/img/nav/third-nav-flower.png" />
-            <HeaderTextElement
-                backgroundImage="/img/nav/fourth-nav-bg.png"
-                text="Contact"
-            />
-            <HeaderImgElement imageSource="/img/nav/fourth-nav-flower.png" />
+            <Logo />
+            <Section backgroundImageSource="/img/nav/first-nav-bg.png" text="About" />
+            <Flower imageSource="/img/nav/first-nav-flower.png" />
+            <Section backgroundImageSource="/img/nav/second-nav-bg.png" text="Team" />
+            <Flower imageSource="/img/nav/second-nav-flower.png" />
+            <Section backgroundImageSource="/img/nav/third-nav-bg.png" text="Projects" />
+            <Flower imageSource="/img/nav/third-nav-flower.png" />
+            <Section backgroundImageSource="/img/nav/fourth-nav-bg.png" text="Contact" />
+            <Flower imageSource="/img/nav/fourth-nav-flower.png" />
         </header>
+    )
+}
+
+function Logo() {
+    return (
+        <div className="flex items-center justify-center bg-white text-midnight ">
+            <h1 className="font-bold text-start sm:text-2xl md:text-4xl lg:text-4xl">
+                Cape⁴
+                <br />
+                labs
+            </h1>
+        </div>
+    )
+}
+
+function Flower({ imageSource: source }: { imageSource: string }) {
+    return (
+        <div className="flex items-center justify-center bg-white text-midnight ">
+            <Image
+                src={source}
+                alt="flower"
+                width={100}
+                height={100}
+                className="h-full w-full"
+                quality={100}
+            />
+        </div>
+    )
+}
+
+function Section({ backgroundImageSource, text }: { backgroundImageSource: string; text: string }) {
+    return (
+        <div className="relative h-full flex items-center justify-center overflow-hidden sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+            <Image
+                src={backgroundImageSource}
+                alt="abstract background"
+                fill
+                className="object-cover absolute inset-0 z-0"
+                unoptimized
+            />
+            <div className="relative z-10 sm:px-2 md:px-2 lg:px-2 xl:px-4">
+                <h2 className="text-white text-start sm:text-xl md:text-xl lg:text-2xl xl:text-3xl">
+                    {text}
+                </h2>
+            </div>
+        </div>
     )
 }
