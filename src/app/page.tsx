7cbbfb0f7a8c3e9580c1c431a42637ffe6565e49
isgin01 <isgin01@gmail.com>
2025-12-23 00:33:12 +0500

    Add <Team /> block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import About from "../components/About"
+import Team from "../components/Team"
 
 export default function Home() {
     return (
         <div className="flex flex-col h-screen overflow-y-scroll bg-white">
             <Header />
             <About />
+            <Team />
         </div>
     )
 }
