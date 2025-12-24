b8c3b8c683a220b3a82213edf77ff6725851dbfc
qwaqd <qwaq.dev@gmail.com>
2025-12-24 22:35:12 +0500

    Correct folders animation

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -5,7 +5,7 @@ const ASSETS = {
 export default function Artwork() {
     return (
         <div className="h-324 w-full flex flex-col items-center relative">
-            <img src={ASSETS.folders} className="w-64 h-auto absolute top-32 z-10" />
+            <img src={ASSETS.folders} className="w-64 h-auto absolute top-32 z-9" />
             <img
                 src={ASSETS.hand}
                 className="w-full h-auto absolute bottom-32 object-contain z-0"
